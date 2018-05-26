5d86c8aabee1d02717a509d4b2fc2bdf3d770844
Rafael Gontijo Winter <rafael@rafaelgontijo.com>
2018-05-25 22:46:01 -0300

    add redis

--- build_dev_machine.sh
+++ build_dev_machine.sh
@@ -14,6 +14,7 @@ apt-get install -y build-essential python3-dev python-dev openssh-client curl
 apt-get install -y --no-install-recommends autoconf automake bzip2 dpkg-dev file g++ gcc imagemagick libbz2-dev libc6-dev libcurl4-openssl-dev libdb-dev libevent-dev libffi-dev libgdbm-dev libgeoip-dev libglib2.0-dev libjpeg-dev libkrb5-dev liblzma-dev libmagickcore-dev libmagickwand-dev libncurses5-dev libncursesw5-dev libpng-dev libpq-dev libreadline-dev libsqlite3-dev libssl-dev libtool libwebp-dev libxml2-dev libxslt-dev libyaml-dev make patch xz-utils zlib1g-dev libmysqlclient-dev
 apt-get install -y postgresql
 apt-get install -y mysql-server
+apt-get install -y redis-server
 apt-get install -y nodejs nodejs-legacy npm
 apt-get install -y vim
 apt-get install -y fonts-droid-fallback fonts-noto fonts-noto-mono
